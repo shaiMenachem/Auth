6182b5f79a3cb55d4066d23c4e1cd62d0298bd2c
shaiMenachem <shai20453@gmail.com>
2024-07-15 15:16:23 +0300

    Feat(auth service)

--- server/config/index.js
+++ server/config/index.js
@@ -1,8 +1,8 @@
 const config = {
   keycloak: {
-    baseUrl: 'http://localhost:8080',
-    clientId: 'shai',
-    clientSecret: '4zfU50qa38Dbeee2ixOdqDxGLNcozEL4'
+    baseUrl: `http://${process.env.keycloakIp}:8000`,
+    clientId: process.env.clientId,
+    clientSecret: process.env.clientSecret
   },
 };
 
