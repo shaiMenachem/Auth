f56ed4f730956d8b6a4e9c5456353061a4766964
shaiMenachem <shai20453@gmail.com>
2024-07-15 15:11:58 +0300

    Feat(auth service)

--- server copy/config/index.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  keycloak: {
-    baseUrl: 'http://localhost:8080',
-    clientId: 'shai',
-    clientSecret: '4zfU50qa38Dbeee2ixOdqDxGLNcozEL4'
-  },
-};
-
-export default config;
